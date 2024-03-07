4d797f35f4b898c79dfe810acf7b610b97e8ac14
MauricioTejada <mauriciotejada14@gmail.com>
2024-03-07 15:26:57 -0300

    fix bug deploy 02

---
+++ env.js
@@ -0,0 +1,17 @@
+// Variables para Deploy
+// const env = {
+//   VITE_FB_API_KEY: "AIzaSyD3ToUSR1jwFA5oUswh7AlCij7HZRjzyk0",
+//   VITE_HOST: "https://pfingenia-production.up.railway.app",
+// };
+
+// Variables para Desarrollo
+const env = {
+  // VITE_FB_API_KEY: "AIzaSyD3ToUSR1jwFA5oUswh7AlCij7HZRjzyk0",
+  VITE_FB_API_KEY: "AIzaSyAchDDBm5zx1tEq_V6q1DQ-8592YN774FY",
+
+  //* ésta ea la dirección principal de la API a la cual se apunta
+  // VITE_HOST: "http://localhost:3002",
+  VITE_HOST: "https://ingenia-v2-backend.onrender.com",
+};
+
+export default env;

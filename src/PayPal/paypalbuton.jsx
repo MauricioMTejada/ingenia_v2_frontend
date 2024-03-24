962d2a79f8953519157bfacd3c6d1837a7b79370
MauricioTejada <mauriciotejada14@gmail.com>
2024-03-24 13:23:15 -0300

    disable Paypal button, fix style carrouserll

--- src/PayPal/paypalbuton.jsx
+++ src/PayPal/paypalbuton.jsx
@@ -8,10 +8,17 @@ export default function Paypalbutton() {
 	const totalpagar = useSelector((state) => state.totalCarrito);
 	const idUser = localStorage.getItem("idUser");
 
+	// console.log(idUser);
+	// console.log(totalpagar);
+
+	// console.log("Tipo de dato de idUser:", typeof idUser, idUser);
+    // console.log("Tipo de dato de totalpagar:", typeof totalpagar, totalpagar);
+
+
 	const handlePaymentSuccess = async () => {
 		try {
 			const response = await axios.post(
-				`${env.VITE_HOST}/buy?costo=${totalpagar}&idUser=${idUser}`
+				`${env.VITE_HOST}/buy/?costo=${totalpagar}&idUser=${idUser}`
 			);
 			window.location.href = response.data.links[1].href;
 		} catch (error) {
@@ -21,9 +28,10 @@ export default function Paypalbutton() {
 
 	return (
 		<Button
+			disabled
 			variant="contained"
 			color="primary"
-			sx={{ color: "white", fontSize: 19, margin: "2rem 0" }}
+			sx={{ color: "white", fontSize: 19, margin: "2rem 0", cursor: "not-allowed" }}
 			onClick={handlePaymentSuccess}
 			startIcon={<PaymentIcon sx={{ color: "white" }} />}>
 			pagar con paypal
